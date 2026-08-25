d1101aae4ab61ba3b356c0201e0b995a92f462d5
chaitanyareddyalavalapati <chaitanyareddyalavalapati>
2026-08-25 17:16:26 +0530

    DAY6

---
+++ rtl/generate.sv
@@ -0,0 +1,13 @@
+module vector_and (
+input wire [7:0] a,
+input wire [7:0] b,
+output wire [7:0] y
+);
+genvar i;
+generate
+for (i = 0; i < 8; i = i + 1) begin : GEN_AND
+assign y[i] = a[i] & b[i];
+end
+endgenerate
+endmodule
+
